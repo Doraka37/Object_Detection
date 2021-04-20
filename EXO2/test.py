bb215e9c19e31e09db52dc55d23b7da981c732ea
unknown <louisdidierjean@yahoo.fr>
2021-04-20 14:01:27 +0200

    Changed display and added text

--- EXO2/test.py
+++ EXO2/test.py
@@ -12,18 +12,26 @@ hog = cv2.HOGDescriptor()
 hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 # load the image and resize it
 image = cv2.imread("./macron.jpg", cv2.IMREAD_COLOR)
-image = imutils.resize(image, width=min(400, image.shape[1]))
+resized_image = imutils.resize(image, width=min(400, image.shape[1]))
 
 # detect people in the image
 start = datetime.datetime.now()
-(rects, weights) = hog.detectMultiScale(image)
+(rects, weights) = hog.detectMultiScale(resized_image)
 print("[INFO] detection took: {}s".format(
 	(datetime.datetime.now() - start).total_seconds()))
 # draw the original bounding boxes
 for (x, y, w, h) in rects:
-	cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+	resized_x = (x / resized_image.shape[1]) * image.shape[1]
+	resized_y = (y / resized_image.shape[0]) * image.shape[0]
+	resized_w = (w / resized_image.shape[1]) * image.shape[1]
+	resized_h = (h / resized_image.shape[0]) * image.shape[0]
+	cv2.rectangle(image, (int(resized_x), int(resized_y)), (int(resized_x) + int(resized_w), int(resized_y) + int(resized_h)), (0, 255, 0), 2)
 
-plt.imshow(image,),plt.show()
+number_of_people = len(rects)
+
+cv2.putText(image, f"{number_of_people} people detected", (5, image.shape[0] - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2)
+
+#plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB)),plt.show()
 # show the output image
-#cv2.imshow("Detections", image)
-#cv2.waitKey(0)
+cv2.imshow("Detections", image)
+cv2.waitKey(0)
