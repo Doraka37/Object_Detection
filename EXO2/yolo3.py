cacca4390abc21b5bf10792e151380926d6fa2d1
Thomas <thomas1.vigier@epitech.eu>
2021-05-06 17:56:51 +0200

    psushing correction of exo

---
+++ EXO2/yolo3.py
@@ -0,0 +1,67 @@
+import numpy as np
+import argparse
+import time
+import cv2
+import os
+from matplotlib import pyplot as plt
+
+model = 'yolov2.weights'
+config = 'yolov2.cfg'
+net = cv2.dnn.readNetFromDarknet(config, model)
+
+labelsPath = os.path.sep.join(["yolo-coco", "coco.names"])
+LABELS = open(labelsPath).read().strip().split("\n")
+
+np.random.seed(42)
+COLORS = np.random.randint(0, 255, size=(len(LABELS), 3),
+	dtype="uint8")
+
+
+image = cv2.imread("./peoples.jpeg")
+(H, W) = image.shape[:2]
+
+ln = net.getLayerNames()
+ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+
+blob = cv2.dnn.blobFromImage(image, 1 / 255.0, (416, 416),
+	swapRB=True, crop=False)
+net.setInput(blob)
+start = time.time()
+layerOutputs = net.forward(ln)
+end = time.time()
+
+print("[INFO] YOLO took {:.6f} seconds".format(end - start))
+
+boxes = []
+confidences = []
+classIDs = []
+conf = 0.3
+
+
+for output in layerOutputs:
+	for detection in output:
+		scores = detection[5:]
+		classID = np.argmax(scores)
+		confidence = scores[classID]
+		if confidence > conf:
+			box = detection[0:4] * np.array([W, H, W, H])
+			(centerX, centerY, width, height) = box.astype("int")
+			x = int(centerX - (width / 2))
+			y = int(centerY - (height / 2))
+			boxes.append([x, y, int(width), int(height)])
+			confidences.append(float(confidence))
+			classIDs.append(classID)
+
+idxs = cv2.dnn.NMSBoxes(boxes, confidences, conf,
+	10)
+
+if len(idxs) > 0:
+	for i in idxs.flatten():
+		(x, y) = (boxes[i][0], boxes[i][1])
+		(w, h) = (boxes[i][2], boxes[i][3])
+		color = [int(c) for c in COLORS[classIDs[i]]]
+		cv2.rectangle(image, (x, y), (x + w, y + h), color, 2)
+		text = "{}: {:.4f}".format(LABELS[classIDs[i]], confidences[i])
+		cv2.putText(image, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX,
+			0.5, color, 2)
+plt.imshow(image,),plt.show()
